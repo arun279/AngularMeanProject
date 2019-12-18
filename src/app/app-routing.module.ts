bf306bb0e81c5e40688c08b3c8ff41bbf1d22daa
Arun Krishnamurthy <infinite279@gmail.com>
2019-12-18 09:30:29 -0500

    adding user components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'add-student' },
+  { path: 'add-student', component: AddUserComponent },
+  { path: 'edit-student/:id', component: EditUserComponent },
+  { path: 'students-list', component: UsersListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent,
+    EditUserComponent,
+    UsersListComponent
   ],
   imports: [
     BrowserModule,
